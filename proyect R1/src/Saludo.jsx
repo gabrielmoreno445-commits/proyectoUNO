70a16ed60ec74e8599dca53ee5bb0251f9addd37
Gaby Moreno <gabrielmoreno445@gmail.com>
2026-03-17 18:03:08 -0300

    cambio en el saludo

--- proyect R1/src/Saludo.jsx
+++ proyect R1/src/Saludo.jsx
@@ -3,7 +3,7 @@ function Saludo() {
   return (
     <div style={{ backgroundColor: '#2e303a', padding: '10px', borderRadius: '8px', color: 'white' }}>
       <h2>¡Soy un componente separado! 😎</h2>
-      <p>Gabriel, esto me permite reutilizar código.</p>
+      <p>Gabriel, actualizando en mi proyecto Git.</p>
     </div>
   )
 }
